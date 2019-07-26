aa9a7ec83e97804bb3c36fc21438503bc99f545a
Juan <50727856+OneHellsrage@users.noreply.github.com>
2019-07-25 20:20:36 -0500

    Add files via upload

---
+++ T3C1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3_1
+{
+    class T3C1
+    {
+        static void Main()
+        {
+            // Variables
+            Console.WriteLine("Por favor ingrese el valor de su salario actual: ");
+            int Salario = int.Parse(Console.ReadLine());
+            int Minimo = 828116;
+            // Condiciones
+            if (Salario < 2 * Minimo)
+            {
+                Console.WriteLine("La tarifa correspondiente es la que pertenece a la categoría A");
+            }
+            else if (2 * Minimo <= Salario && Salario < 4 * Minimo)
+            {
+                Console.WriteLine("La tarifa correspondiente es la que pertenece a la categoría B");
+            }else
+            {
+                Console.WriteLine("La tarifa correspondiente es la que pertenece a la categoría C");
+            }
+        } 
+    }
+}
---
+++ T3C2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3_2
+{
+    class T3C2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de su salario mensual");
+            int Salario = int.Parse(Console.ReadLine());
+            int Minimo = 828116;
+
+            if ( Salario < 2*Minimo)
+            {
+                int A = 3200;
+                Console.WriteLine("La Tarifa correspondiente es A, $: " + A);
+            } else if (2*Minimo <= Salario && Salario <5*Minimo)
+            {
+                int B = 12700;
+                Console.WriteLine("La Tarifa correspondiente es B, $: " + B);
+            }else
+            {
+                int C = 33500;
+                Console.WriteLine("La Tarifa correspondiente es C, $: " + C);
+            }
+        }
+    }
+}

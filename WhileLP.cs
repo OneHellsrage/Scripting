08761b39e3f47f3d2255fc3ca38ec0b2beb78d20
Juan <50727856+OneHellsrage@users.noreply.github.com>
2019-08-08 09:45:31 -0500

    Add files via upload

---
+++ WhileLP.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("ingrese el número de datos: ");
+            int datos = int.Parse(Console.ReadLine());
+            double total = 0;
+            int contador = 0;
+            int maximo = 0;
+            int minimo = 200;
+            string nombreminimo = "";
+            string nombremaximo = "";
+
+            while (contador < datos)
+            {
+                Console.Write("Nombre y edad: ");
+                string nombre = Console.ReadLine();
+                int edad = int.Parse(Console.ReadLine());
+                if (edad >= maximo)
+                {
+                    maximo = edad;
+                    nombremaximo = nombre;
+                }
+                if (edad <= minimo)
+                {
+                    minimo = edad;
+                    nombreminimo = nombre;
+                }
+                total += edad;
+                contador += 1;
+
+                
+            }
+
+            double promedio = total / contador;
+            Console.WriteLine("primedio: " + promedio);
+            Console.WriteLine("Mayor edad: " + maximo);
+            Console.WriteLine("menor edad: " + minimo);
+            Console.WriteLine("nombre del mayor: " + nombremaximo);
+            Console.WriteLine("nombre del menor: " + nombreminimo);
+
+
+        }
+    }
+}

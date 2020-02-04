a26f12895c0ef1719ece43c1b1f513072697a39d
Juan <50727856+OneHellsrage@users.noreply.github.com>
2020-02-04 11:34:00 -0500

    Add files via upload

---
+++ Bicicleta.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bicicleta.Clases
+{
+    class Bicicleta
+    {
+        // Atributos.
+
+        public string Marca;
+        public string Color;
+        public string Material;
+        public float Tamano_Marco = 0f;
+        public float Tamano_Llanta = 0f;
+        public short cantidadCambios;
+        public short CambioActual;
+        public short VelocidadActual;
+        public short VelocidadMaxima;
+
+        // Constructor.
+
+        public Bicicleta()
+        {
+            try
+            {
+            }
+            catch(Exception Error)
+            {
+            }
+        }
+
+        public void Acelerar(short Vel_Final)
+        {
+            try
+            {
+            }
+            catch (Exception Error)
+            {
+            }
+        }
+        public void Subir_Cambio()
+        {
+            try
+            {
+            }
+            catch (Exception Error)
+            {
+            }
+        }
+        public void Subir_Cambio(short Cambio_Final)
+        {
+            try
+            {
+            }
+            catch (Exception Error)
+            {
+            }
+        }
+        public void Bajar_Cambio()
+        {
+            try
+            {
+            }
+            catch (Exception Error)
+            {
+            }
+        }
+        public void Bajar_Cambio(short Cambio_Final)
+        {
+            try
+            {
+            }
+            catch (Exception Error)
+            {
+            }
+        }
+        public void Desacelerar(short Vel_Final)
+        {
+            try
+            {
+            }
+            catch (Exception Error)
+            {
+            }
+        }
+    }
+}
